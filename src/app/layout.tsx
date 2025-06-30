66599654d0743109cf351b280a86f49a7b8e4d54
ranjeet <ranjeetsuthar108@gmail.com>
2025-07-01 01:35:44 +0530

    add sone icon and improve files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "HeartSync - Online Meet",
   description: "Generated HeartSync app",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
